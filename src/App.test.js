ddfd3cd5eac1bb98be02664918b3e77f78de1312
Serhii Fesenko <compt.sergey@gmail.com>
2022-11-25 18:51:50 -0500

    Consequence checker function

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,9 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Renju Game/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+// npm test
+// npm test -- --coverage
--- src/components/Game.js
+++ src/components/Game.js
@@ -35,8 +35,6 @@ class Game extends React.Component {
         
         const col = (i % 15) + 1
         const row = Math.ceil((i + 1)/15)
-        
-        console.log(col, row)
 
         this.setState({
             history: history.concat([
@@ -56,6 +54,25 @@ class Game extends React.Component {
             currentStep: i,
             bNext: !(i % 2)
         })
+    }
+
+    checkWinner(squares, col, row) {
+        // horizontal
+        let result = []
+        for(let j = 0; j<=14; j++) {
+            let i = 15 * (row-1);
+            if(squares[i]!=null) {
+                if(!result) {
+                    result.push(squares[i]);
+                } else {
+                    if(result) {
+
+                    }
+                }
+            }
+        }
+
+        return false
     }
 
     render () {
@@ -80,3 +97,46 @@ class Game extends React.Component {
     }
 }
 export default Game;
+
+const arr = [
+    {id: 15, value: 'white'},
+    {id: 16, value: 'white'},
+    {id: 17, value: 'white'},
+    {id: 18, value: 'white'},
+    {id: 19, value: 'black'},
+    {id: 20, value: 'white'},
+    {id: 21, value: 'white'},
+    {id: 22, value: 'white'},
+    {id: 23, value: 'white'},
+    {id: 24, value: 'white'},
+    {id: 25, value: 'black'},
+    {id: 26, value: 'white'},
+    {id: 27, value: 'white'},
+    {id: 28, value: 'white'},
+    {id: 29, value: 'white'},
+];
+const checkConsequence = arr => {
+   let prev = {
+      element: null,
+      count: 0,
+      elementIds: []
+   };
+   for(let i = 0; i < arr.length; i++){
+      const { count, element } = prev;
+      if(element===arr[i].value) {
+        prev.count = count + 1;
+        prev.elementIds.push(arr[i].id);
+      } else {
+        prev.count = 1;
+        prev.elementIds = [arr[i].id];
+      }
+      prev.element = arr[i].value;
+      if(count === 4 && element === arr[i].value){
+        if(arr[i+1] && arr[i+1].value!=element) {
+            return prev.elementIds;
+        }
+      };
+   };
+   return false;
+};
+console.log(checkConsequence(arr));
