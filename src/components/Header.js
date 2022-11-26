3d22884a1e64ee14f9d9d353e06d8215fe01f326
Serhii Fesenko <compt.sergey@gmail.com>
2022-11-26 14:07:41 -0500

    Mobile styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,11 @@
 import React from "react";
-
+import styles from "./styles/Header.module.css"
 class Header extends React.Component {
     render() {
         return(
-            <div>
+            <div className={styles.Header}>
                 <h1>Renju Game</h1>
+                <p>The first who placed 5 in a row (horizontally, vertically or diagonally) wins.</p>
             </div>
         )
     }
